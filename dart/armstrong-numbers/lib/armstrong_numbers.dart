bd727f312d54c570eb7aab8aa7a45f012494f5c9
s j <sj@Mac-mini.local>
2023-10-02 17:31:09 -0600

    minor cleanup * move comments around * add some comments * eliminate variable which is only used once

--- dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,24 +1,22 @@
 class ArmstrongNumbers {
-  // return x==y
+
+  // check a number to see whether it is an armstrong (aka narcissistic) number
   bool isArmstrongNumber(String noom){
-    // get the length of the string
+
+    // track the total
     BigInt total = BigInt.from(0);
-    int length = noom.length;
 
     // separate the number into digit lists
     List<String> digits = noom.split('');
     
-    // for loop that parses each string into a number
-    // raise each digit to the length of digits
+    // for loop that parses each string digit into a number
     for (final String digit in digits){
-      int number = int.parse(digit);
-      // raise each digit to the number of digits
-      total += BigInt.from(number).pow(length); 
+      BigInt bigint = BigInt.from(int.parse(digit));
+      // raise each digit to the power equal to the number of digits
+      total += bigint.pow(noom.length); 
     }
+
+    // compare the computed total to the original number
     return total.toString() == noom;
-
   }
-  
 }
-
-

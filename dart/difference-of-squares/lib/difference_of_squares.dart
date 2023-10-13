54019f45d46fc0c30d4a94c143a002cc5fb3efa9
Nastia Felt <nfelt@ss-MacBook-Air.local>
2023-10-12 22:15:08 -0600

    difference of squares exercise

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -2,34 +2,33 @@
 import "dart:math";
 
 class DifferenceOfSquares {
-  
-// Find the difference between the square of the sum and the sum of the squares of the first N natural numbers.
-// The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)² = 55² = 3025.
-// The sum of the squares of the first ten natural numbers is 1² + 2² + ... + 10² = 385.
-// Hence the difference between the square of the sum of the first ten natural numbers 
-// and the sum of the squares of the first ten natural numbers is 3025 - 385 = 2640.
-//Finding the best algorithm for the problem is a key skill in software engineering.
 
-  int squareOfSum(int number){
-    var factorial = 1; 
+  // calculate the squared result of numbers added together
+  int squareOfSum(int digit){ 
+    int baseNum = 0;
+    // add the digits together
+    for( int i = digit; i >= 1; i-- ) { 
+        baseNum += i ;
+    }
+    int num1 = pow(baseNum, 2).toInt();
+    return num1;
     
-    for( int i = number ; i >= 1; i-- ) { 
-        factorial *= i ;
-    } 
-    print(factorial);
-    num newNum = pow(factorial, 2);
-    return newNum.toInt();
-
   }
 
-  int sumOfSquares(int number){
-
-    return number;
-
+  // calculate the sum of some squared numbers
+  int sumOfSquares(int digit){
+    int baseNum = 0;
+    for( int i = digit; i >= 0; i-- ) { 
+       print (baseNum);
+       baseNum += pow(i, 2).toInt();
+    }
+     return baseNum;
+    
   }
-  int differenceOfSquares(int number){
-
-    return number;
-  }
+ 
+  // calculate the difference between the results
+  int differenceOfSquares(int digit){
+      return squareOfSum(digit) - sumOfSquares(digit);
+    }
 
 }

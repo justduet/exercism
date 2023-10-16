5f58b46bf6f87fb56b63c750278a1f253cc06b07
Nastia Felt <nfelt@ss-MacBook-Air.local>
2023-10-16 13:06:14 -0600

    make variables static and constant

--- dart/space-age/lib/space_age.dart
+++ dart/space-age/lib/space_age.dart
@@ -1,18 +1,17 @@
 class SpaceAge {
+  static const Map<String, double> orbitalPeriod = {
+    'Mercury':    0.2408467,
+    'Venus'  :    0.61519726,
+    'Mars'   :    1.8808158,
+    'Jupiter':   11.862615,
+    'Saturn' :   29.447498,
+    'Uranus' :   84.016846,
+    'Neptune':  164.7913
+  };
+
+  static const int EARTH_YEAR_SECONDS = 31557600;
+
   static double age({String planet = '', int seconds = 0}) {
-
-    Map<String, double> orbitalPeriod = {
-      'Mercury' :   0.2408467,
-      'Venus'   :   0.61519726,
-      'Mars'    :   1.8808158,
-      'Jupiter' :  11.862615,
-      'Saturn'  :  29.447498,
-      'Uranus'  :  84.016846,
-      'Neptune' : 164.7913
-    };
-
-    const int EARTH_YEAR_SECONDS = 31557600;
-
     // calculate the amount of Earth years from the total # of seconds
     double yearsOnEarth = seconds / EARTH_YEAR_SECONDS;
 

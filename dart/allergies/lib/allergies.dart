80c90fd8c6e09f126e9e8a94ef135f8426ffe620
Nastia Felt <anastasia.felt@gmail.com>
2023-10-23 13:43:53 -0600

    allergies exercise: forEach, bitwiseOperators, check if null

--- dart/allergies/lib/allergies.dart
+++ dart/allergies/lib/allergies.dart
@@ -1,13 +1,30 @@
 class Allergies {
-// So if Tom is allergic to peanuts and chocolate, he gets a score of 34.
+  static Map<String, int> allergenMap = {
+    'eggs': 1,
+    'peanuts': 2,
+    'shellfish': 4,
+    'strawberries': 8,
+    'tomatoes': 16,
+    'chocolate': 32,
+    'pollen': 64,
+    'cats': 128
+  };
 
-// Now, given just that score of 34, your program should be able to say:
-
-// Whether Tom is allergic to any one of those allergens listed above.
-// All the allergens Tom is allergic to.
-  bool allergicTo(String allergen, int value){
-    return false;
-
+  // is someone allergic to something
+  bool allergicTo(String allergen, int score) {
+    // return true if the allergen is found in the submitted value
+    return (allergenMap[allergen] ?? 0) & score > 0;
   }
-  List<String> list(int ){}
+
+  // give back a list of allergens the person is allergic to
+  List<String> list(int score) {
+    List<String> allergens = [];
+    // for each allergen in the allergenMap
+    // check whether the corresponding value's bit is set
+    // (eggs, peanuts, shellfish, strawberries, tomatoes, chocolate, pollen, cats)
+    allergenMap.keys.forEach((String key) {
+      if ((allergenMap[key] ?? 0) & score > 0) allergens.add(key);
+    });
+    return allergens;
+  }
 }

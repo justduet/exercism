bb8a1b41b8462900c8fe1d15c03ddc7e32f23169
Nastia Felt <anastasia.felt@gmail.com>
2023-10-25 15:04:00 -0600

    cleaned code

--- dart/gigasecond/lib/gigasecond.dart
+++ dart/gigasecond/lib/gigasecond.dart
@@ -1,7 +1,6 @@
 DateTime addGigasecondTo(final DateTime dateTime) {
   // add a gigasecond to the universal time that is passed in
-  final newdateTime = dateTime.add(const Duration(seconds: 1000000000));
-  return newdateTime;
+  return dateTime.add(const Duration(seconds: 1000000000));
 }
 
 

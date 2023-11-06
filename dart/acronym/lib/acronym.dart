afe4301e05e0cff726e73b89049184a30d2cd904
Nastia Felt <anastasia.felt@gmail.com>
2023-11-06 11:16:31 -0700

    cleanup

--- dart/acronym/lib/acronym.dart
+++ dart/acronym/lib/acronym.dart
@@ -1,27 +1,12 @@
 class Acronym {
   String abbreviate(String statement) {
-
-    // change hyphens to spaces
-    String hyphensGone = statement.replaceAll('-', ' ');
-
-    // get rid of all unneccesary punctuation
-    String words = hyphensGone.replaceAll(RegExp(r'([^\s\w]|[_])'), '');
-    String words1 = words.replaceAll(RegExp(r'(\s+)'), ' ');
-    print (words);
-    print(words1);
-
-    // split words into separate lists
-    List<String> separateWords = words1.split(' ');
-    List<String> letterList = [];
-
-    // capitalize and add each first letter of each word to a list
-    for (String s in separateWords) {
-      letterList.add((s[0].toUpperCase()));
-    }
-
-    final joinedAcronym = letterList.join('');
-    print(joinedAcronym);
-
-    return (joinedAcronym);
+    // change multiple hyphens, underscores, and spaces to single spaces
+    // split into words
+    // take the first letter of each word
+    // create the acronym
+    return statement.replaceAll(RegExp(r'(_|-|\s)+'), ' ')
+      .split(' ')
+      .map((word)=> word[0].toUpperCase()).join('');
   }
 }
+

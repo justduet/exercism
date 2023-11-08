091d3cc467733b30fc0ca308a488b391bec6099e
Nastia Felt <anastasia.felt@gmail.com>
2023-11-08 14:06:14 -0700

    Added map for optimization

--- dart/resistor-color/lib/resistor_color.dart
+++ dart/resistor-color/lib/resistor_color.dart
@@ -1,17 +1,18 @@
 class ResistorColor {
   // resistor colors
   // each color's index is the color's value
-  final colors = <String>[
-    'black',
-    'brown',
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'violet',
-    'grey',
-    'white',
-  ];
-  int colorCode(String color) => colors.indexOf(color);
+  final Map<String, int> colorMap = {
+    'black' : 0,
+    'brown' : 1,
+    'red'   : 2,
+    'orange': 3,
+    'yellow': 4,
+    'green' : 5,
+    'blue'  : 6,
+    'violet': 7,
+    'grey'  : 8,
+    'white' : 9
+  };
+  int colorCode(String color) => colorMap[color]!;
+  List<String> get colors => colorMap.keys.toList();
 }
